a21a58fe1c3b5e639aca8ffe0b8ae29400bc6e80
lselbeck <lselbeck@gmail.com>
2018-04-16 11:10:56 -0700

    trying again

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const mailService = require('backend-tools').mailService;
-const emailerPassword = require('./src/keys').emailerPassword;
+const emailerPassword = require('./keys').emailerPassword;
 
 const app = express();
 const port = process.env.PORT || 5000;
