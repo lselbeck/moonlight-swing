fb158fb9fb3d88d6ca24cd507f04c2aec083d704
lselbeck <lselbeck@gmail.com>
2019-10-05 09:33:22 -0700

    wipe it all!

--- src/components/ScrollLink/ScrollLink.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { Link } from 'react-scroll'
-
-const ScrollLink = (props) => <Link {...props} spy={true} smooth={true} duration={800}>{props.children}</Link>
-
-export default ScrollLink
